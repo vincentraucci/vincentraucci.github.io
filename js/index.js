51a66033865429d8d79ac0bf70916db94f6ab2fe
Michael Peluso <mp272@njit.edu>
2023-08-08 00:25:24 -0400

    add store information page

--- js/index.js
+++ js/index.js
@@ -1,26 +1,30 @@
 $(window).scroll(function () {
-    if ($(window).scrollTop() > $(".landing-video").offset().top + $(".landing-video").outerHeight() * 0.75) {
+    if (window.location.href.includes("index.html")) {
+        if ($(window).scrollTop() > $(".landing-video").offset().top + $(".landing-video").outerHeight() * 0.75) {
+            $("#main-nav").addClass("bg-black").css({ "text-shadow": "none" });
+            $(".dropdown-menu").addClass("bg-black").removeClass("bg-transparent");
+
+            $(".navbar-toggler").on("click", function () {
+                $("#main-nav").addClass("bg-black");
+            });
+        } else {
+            $("#main-nav").removeClass("bg-black").css({ "text-shadow": "black 0px 0 20px" });
+            $(".dropdown-menu").removeClass("bg-black").addClass("bg-transparent");
+        }
+    } else {
         $("#main-nav").addClass("bg-black").css({ "text-shadow": "none" });
         $(".dropdown-menu").addClass("bg-black").removeClass("bg-transparent");
-
-        $(".navbar-toggler").on("click", function () {
-            $("#main-nav").addClass("bg-black");
-        });
-    } else {
-        $("#main-nav").removeClass("bg-black").css({ "text-shadow": "black 0px 0 20px" });
-        $(".dropdown-menu").removeClass("bg-black").addClass("bg-transparent");
     }
 });
-
-if ($(window).width() < 768) {
-    $(".landing-video").removeClass("img-fluid");
-} else {
-}
-
-$("#video-cover").height($(".landing-video").height() - $("#main-nav").height());
 
 $(".toggleImage").click(function () {
     const isRotated = $(".toggleImage").data("rotated");
     $(".toggleImage").css("transform", isRotated ? "rotate(0deg)" : "rotate(45deg)");
     $(".toggleImage").data("rotated", !isRotated);
 });
+
+$("#video-cover").height($(".landing-video").height() - $("#main-nav").height());
+
+if ($(window).width() < 768) {
+    $(".landing-video").removeClass("img-fluid");
+}
