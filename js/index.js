189e6082128cf87f390d34f516af6baff57dc3a7
Michael Peluso <mp272@njit.edu>
2023-08-10 02:14:25 -0400

    create meet_the_owner page

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 $(window).scroll(function () {
     if (window.location.href.includes("index.html")) {
         if ($(window).scrollTop() > $(".landing-video").offset().top + $(".landing-video").outerHeight() * 0.75) {
-            $("#main-nav").addClass("bg-black").css({ "text-shadow": "none" });
+            $("#main-nav").addClass("bg-black").css({ "text-shadow": "none" }).addClass("nav-shadow");
             $(".dropdown-menu").addClass("bg-black").removeClass("bg-transparent");
 
             $(".navbar-toggler").on("click", function () {
                 $("#main-nav").addClass("bg-black");
             });
         } else {
-            $("#main-nav").removeClass("bg-black").css({ "text-shadow": "black 0px 0 20px" });
+            $("#main-nav").removeClass("bg-black").css({ "text-shadow": "black 0px 0 20px" }).removeClass("nav-shadow");
             $(".dropdown-menu").removeClass("bg-black").addClass("bg-transparent");
         }
     } else {
@@ -28,3 +28,10 @@ $("#video-cover").height($(".landing-video").height() - $("#main-nav").height())
 if ($(window).width() < 768) {
     $(".landing-video").removeClass("img-fluid");
 }
+
+$(function () {
+    $(".pop").on("click", function () {
+        $(".imagepreview").attr("src", $(this).attr("src"));
+        $("#imagemodal").css("display: block;");
+    });
+});
