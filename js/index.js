61c91945d56dc603d2a6ef90fed353367d96aa1f
Michael Peluso <mp272@njit.edu>
2023-08-11 02:54:43 -0400

    complete meet the owner page and build template for faq page

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ $(window).scroll(function () {
     if (window.location.href.includes("index.html")) {
         if ($(window).scrollTop() > $(".landing-video").offset().top + $(".landing-video").outerHeight() * 0.75) {
             $("#main-nav").addClass("bg-black").css({ "text-shadow": "none" }).addClass("nav-shadow");
-            $(".dropdown-menu").addClass("bg-black").removeClass("bg-transparent");
+            $(".dropdown-menu").addClass("bg-black").removeClass("bg-transparent").addClass("nav-shadow");
 
             $(".navbar-toggler").on("click", function () {
                 $("#main-nav").addClass("bg-black");
             });
         } else {
             $("#main-nav").removeClass("bg-black").css({ "text-shadow": "black 0px 0 20px" }).removeClass("nav-shadow");
-            $(".dropdown-menu").removeClass("bg-black").addClass("bg-transparent");
+            $(".dropdown-menu").removeClass("bg-black").addClass("bg-transparent").removeClass("nav-shadow");
         }
     } else {
         $("#main-nav").addClass("bg-black").css({ "text-shadow": "none" });
@@ -18,9 +18,10 @@ $(window).scroll(function () {
 });
 
 $(".toggleImage").click(function () {
-    const isRotated = $(".toggleImage").data("rotated");
-    $(".toggleImage").css("transform", isRotated ? "rotate(0deg)" : "rotate(45deg)");
-    $(".toggleImage").data("rotated", !isRotated);
+    const isRotated = $(this).data("rotated");
+    $(this)
+        .css("transform", isRotated ? "rotate(0deg)" : "rotate(45deg)")
+        .data("rotated", !isRotated);
 });
 
 $("#video-cover").height($(".landing-video").height() - $("#main-nav").height());
@@ -28,10 +29,3 @@ $("#video-cover").height($(".landing-video").height() - $("#main-nav").height())
 if ($(window).width() < 768) {
     $(".landing-video").removeClass("img-fluid");
 }
-
-$(function () {
-    $(".pop").on("click", function () {
-        $(".imagepreview").attr("src", $(this).attr("src"));
-        $("#imagemodal").css("display: block;");
-    });
-});
