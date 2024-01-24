48936c782510b21a5d6d4f87bdcaeb5da35176c7
Michael Peluso <mp272@njit.edu>
2024-01-24 02:43:09 -0500

    fixing nav transition

--- js/index.js
+++ js/index.js
@@ -27,46 +27,33 @@ $(document).ready(function () {
         });
 
     /*
-    Navbar Scroll Effect
+     Navbar Scroll Effect
     */
-    var $mainNav = $("#main-nav");
-    var $dropdownMenu = $(".dropdown-menu");
-    var navOpen = false;
+    if (window.location.href.includes("index.html")) {
+        var navOpen = false;
 
-    function updateNavOnScroll() {
-        console.log(navOpen);
-        if (window.location.href.includes("index.html")) {
-            if ($(window).scrollTop() === 0 && !navOpen) {
-                $mainNav.removeClass("bg-black").removeClass("nav-shadow");
-                $dropdownMenu.removeClass("bg-black").addClass("bg-transparent").removeClass("nav-shadow");
-            } else {
-                $mainNav.addClass("bg-black").addClass("nav-shadow");
-                $dropdownMenu.addClass("bg-black").removeClass("bg-transparent").addClass("nav-shadow");
+        $(window).scroll(function () {
+            if ($(window).scrollTop() <= 0) {
+                $("#main-nav").toggleClass("bg-black", navOpen);
             }
-        }
+        });
+
+        $(document).on("click", ".navbar-toggler", function () {
+            navOpen = !navOpen;
+            if ($(window).scrollTop() <= 0) {
+                $("#main-nav").toggleClass("bg-black", navOpen);
+            }
+            handleRotationEffect();
+        });
     }
-    updateNavOnScroll();
-    $(window).scroll(updateNavOnScroll);
 
-    $(".navbar-toggler").click(function () {
-        navOpen = !navOpen;
-        console.log("clicled");
-        if ($(window).scrollTop() === 0) {
-            $mainNav.addClass("bg-black");
-            if (!navOpen) {
-                $mainNav.removeClass("bg-black");
-            }
-        }
-        $(".toggleImage").click();
-    });
+    // Menu Button Spin Animation
+    $(".toggleImage").click(handleRotationEffect);
 
-    /*
-    Menu Button Spin Animation
-    */
-    $(".toggleImage").click(function () {
-        const isRotated = $(this).data("rotated");
-        $(this)
+    function handleRotationEffect() {
+        const isRotated = $(".toggleImage").data("rotated");
+        $(".toggleImage")
             .css("transform", isRotated ? "rotate(0deg)" : "rotate(45deg)")
             .data("rotated", !isRotated);
-    });
+    }
 });
