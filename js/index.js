4ab2cb13b99030c16712b6c7e45d4aacc0890939
Michael Peluso <mp272@njit.edu>
2024-05-21 16:30:52 -0400

    update nav transparency

--- js/index.js
+++ js/index.js
@@ -19,34 +19,13 @@ $(document).ready(function () {
             } else {
                 $("#nav-" + currentFile).addClass("active");
             }
+            updateNavScroll();
         });
     fetch("/templates/footer.html")
         .then((response) => response.text())
         .then((data) => {
             $("footer").html(data);
         });
-
-    /*
-     Navbar Scroll Effect
-    */
-    if (window.location.href.includes("index.html")) {
-        var navOpen = false;
-        $("#main-nav").addClass("bg-transparent");
-
-        $(window).scroll(function () {
-            if ($(window).scrollTop() <= 0) {
-                $("#main-nav").toggleClass("bg-transparent", navOpen);
-            }
-        });
-
-        $(document).on("click", ".navbar-toggler", function () {
-            navOpen = !navOpen;
-            if ($(window).scrollTop() <= 0) {
-                $("#main-nav").toggleClass("bg-black", navOpen);
-            }
-            handleRotationEffect();
-        });
-    }
 
     // Menu Button Spin Animation
     $(".toggleImage").click(handleRotationEffect);
@@ -58,3 +37,47 @@ $(document).ready(function () {
             .data("rotated", !isRotated);
     }
 });
+
+// Navbar Scroll Effect
+function updateNavScroll() {
+    if (window.location.href == "http://theguitarshop.co/" || window.location.href == "http://127.0.0.1:5500/") {
+        var navOpen = false;
+        $("nav").find("*").css("transition", "background-color 100ms linear, nav-shadow 100ms linear");
+        $("nav").find("*").addClass("bg-transparent").removeClass("nav-shadow");
+
+        $(window).scroll(function () {
+            var atTop = $(window).scrollTop() <= 0;
+            $("nav").find("*").toggleClass("bg-transparent", atTop);
+            $("#main-nav").toggleClass("nav-shadow", !atTop);
+            $("dropdown-menu").toggleClass("nav-shadow", !atTop);
+        });
+
+        $("nav")
+            .mouseenter(function () {
+                $("nav").find("*").removeClass("bg-transparent");
+                $("#main-nav").addClass("nav-shadow");
+                $("dropdown-menu").addClass("nav-shadow");
+            })
+            .mouseleave(function () {
+                if ($(window).scrollTop() <= 0) {
+                    $("nav").find("*").addClass("bg-transparent").removeClass("nav-shadow");
+                }
+            });
+
+        $(document).on("click", ".navbar-toggler", function () {
+            navOpen = !navOpen;
+            if ($(window).scrollTop() <= 0) {
+                $("#main-nav").toggleClass("bg-transparent", navOpen).toggleClass("nav-shadow", navOpen);
+            }
+            handleRotationEffect();
+        });
+    }
+}
+
+// google reviews
+function fetchReviews() {
+    fetch("https://mybusiness.googleapis.com/v4/accounts/{accountId}/locations/{locationId}/reviews")
+        .then((response) => console.log(response))
+        .then((data) => {})
+        .catch((error) => {});
+}
